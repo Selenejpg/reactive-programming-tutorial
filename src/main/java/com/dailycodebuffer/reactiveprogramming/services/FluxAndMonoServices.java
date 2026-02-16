91738d7d7de41a7d7581ef2e0879d8c704ca8516
Noemi Pintaldi <n.pintaldi@contractors.crifgroup.com>
2026-02-16 16:38:16 +0100

    Writing the first Flux Writing the first Mono Logging Reactive Streams JUnit testing for Flux and Mono

---
+++ src/main/java/com/dailycodebuffer/reactiveprogramming/services/FluxAndMonoServices.java
@@ -0,0 +1,23 @@
+package com.dailycodebuffer.reactiveprogramming.services;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class FluxAndMonoServices {
+
+    public Flux<String> fruitsFlux() {
+        return Flux.fromIterable(List.of("Mango", "Orange", "Banana")).log();
+    }
+
+    public Mono<String> fruitsMono() {
+        return Mono.just("Mango").log();
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoServices fluxAndMonoServices = new FluxAndMonoServices();
+        fluxAndMonoServices.fruitsFlux().subscribe(s -> System.out.println("s = " + s));
+        fluxAndMonoServices.fruitsMono().subscribe(s -> System.out.println("Mono s = " + s));
+    }
+}
---
+++ src/test/java/com/dailycodebuffer/reactiveprogramming/services/FluxAndMonoServicesTest.java
@@ -0,0 +1,25 @@
+package com.dailycodebuffer.reactiveprogramming.services;
+
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
+class FluxAndMonoServicesTest {
+
+    FluxAndMonoServices fluxAndMonoServices = new FluxAndMonoServices();
+
+    @Test
+    void fruitsFlux() {
+        var fruitsFlux = fluxAndMonoServices.fruitsFlux();
+        StepVerifier.create(fruitsFlux)
+                .expectNext("Mango", "Orange", "Banana")
+                .verifyComplete();
+    }
+
+    @Test
+    void fruitsMono() {
+        var fruitsMono = fluxAndMonoServices.fruitsMono();
+        StepVerifier.create(fruitsMono)
+                .expectNext("Mango")
+                .verifyComplete();
+    }
+}
